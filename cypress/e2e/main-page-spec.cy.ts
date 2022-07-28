d86dd0430a1e57afc2de76a8943dd5af45fa3354
Catlyn Bowles <bowlescatlyn@gmail.com>
2022-07-28 16:39:22 -0600

    Add single book fixture

--- cypress/e2e/main-page-spec.cy.ts
+++ cypress/e2e/main-page-spec.cy.ts
@@ -3,9 +3,24 @@ import cypress from "cypress"
 describe('Top Book Page', () => {
   beforeEach(() => {
     cy.visit('http://localhost:3000')
+    cy.intercept('GET', 'https://api.nytimes.com/svc/books/v3/lists/full-overview.json?api-key=tBjYHYybf8UG944wMFG4Hn44NXmN9Lyj', {fixture: "bookData"})
   })
+
+  it('Should have a page title', () => {
+    cy.get('h1').should('contain.text', 'Curious Reader')
+  })
+
+  it('Should have a home page displaying books', () => {
+    cy.get('.book-card').should('contain.text', 'Curious Reader')
+  })
+
 
   it('should test if true is true', () => {
     expect(true).to.equal(true)
   })
 })
+
+// check the length of the fixture
+// see a home page with previews
+// select different book genres/ filtered
+// go back to home page
