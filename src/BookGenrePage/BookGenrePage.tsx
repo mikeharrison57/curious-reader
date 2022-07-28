eb8f5d291345d075fd14807c12b3b54b109bc95a
Michael J. Harrison <95496577+mikeharrison57@users.noreply.github.com>
2022-07-28 09:10:16 -0600

    Genre books maintained after refresh

--- src/BookGenrePage/BookGenrePage.tsx
+++ src/BookGenrePage/BookGenrePage.tsx
@@ -11,12 +11,25 @@ interface Props {
 
 const BookGenrePage = ({listName}: Props) => {
 
-  const [ list, setList ] = useState({})
+  const [ list, setList ] = useState({
+    "list_name": "",
+    "list_name_encoded": "",
+    "bestsellers_date": "",
+    "published_date": "",
+    "published_date_description": "",
+    "next_published_date": "",
+    "previous_published_date": "",
+    "display_name": "",
+    "normal_list_ends_at": 0,
+    "updated": "",
+    "books": []
+  })
+
+  // const [ selectedList ] = useState(listName)
 
   const getListData = () => {
     fetchBooksByGenre(listName)
     .then((data) => { 
-      console.log(data)
       setList(data.results)
     })
   }
@@ -24,10 +37,17 @@ const BookGenrePage = ({listName}: Props) => {
   useEffect(() => {
     getListData()
   }, [listName])
-  
+
+  const selecetedGenreBooks =  list.books.map((book) => {
+    return (
+      <BookCard key={book.isbns[1]} book={book}/>
+      // key={book.isbns[1]} 
+    )
+  })
+
   return (
     <section className='selected-genre-books'>
-      {/* {selectedGenreBooks} */}
+      {selecetedGenreBooks}
     </section>
   )
 }
