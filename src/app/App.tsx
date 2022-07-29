498a5d598dd10eecc018a695201e947fc5926eb0
Megan Schuetz <mschuetz411@gmail.com>
2022-07-29 09:50:42 -0600

    fixed styling on book container

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -27,7 +27,7 @@ class App extends React.Component<{}, IState> {
   
   render = () => {
     return (
-      <>
+    <body>
       {this.state.error ? <div><ErrorMessage /></div> :
         <main>
           <nav>
@@ -42,12 +42,12 @@ class App extends React.Component<{}, IState> {
       
         <Route exact path='/:list_name' render={({match}) => {
           return (
-            <BookGenrePage listName={match.params.list_name} error={this.state.error}/>
+          <BookGenrePage listName={match.params.list_name} error={this.state.error}/>
           )
           }}/>
         </main>
         }
-      </>
+    </body>
     )
   };
 };
