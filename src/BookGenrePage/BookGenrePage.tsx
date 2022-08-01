2fb97cdf765f048d101c759ae9b7a0703ec87cab
Nick Orlov <ialwaysreply@gmail.com>
2022-08-01 12:47:36 -0700

    FIx the useEffect syntax, refactor

--- src/BookGenrePage/BookGenrePage.tsx
+++ src/BookGenrePage/BookGenrePage.tsx
@@ -1,11 +1,9 @@
 import React, { useState, useEffect } from 'react';
-import { fetchBooksByGenre } from '../api-calls';
 import BookCard from '../BookCard/BookCard';
 import '../BookGenrePage/BookGenrePage.css'
 import ErrorMessage from '../ErrorMessage/ErrorMessage';
 import loadingIcon from '../assets/orange-loading.gif';
 import { IState, IList } from '../Interfaces';
-
 
 interface Props {
   listName: string
@@ -28,13 +26,11 @@ let defaultList = {
 }
 
 const BookGenrePage = ({listName, genres, error}: Props) => {
-
   const [ list, setList ] = useState(defaultList);
 
-  const [genreError, setGenreError] = useState(error);
+  const [genreError] = useState(error);
 
   const getListData = () => {
-    console.log( 'list name', listName)
       const selectedGenre: IList = genres.bookLists.find(genre => genre.list_name === listName)
       setList(selectedGenre)
   };
@@ -42,24 +38,7 @@ const BookGenrePage = ({listName, genres, error}: Props) => {
   useEffect(() => {
     console.log('list', list)
       getListData()
-      // return clearEffect();
-  }, [genres]);
-
-  // const clearEffect = () => {
-  //   setList({
-  //     "list_name": "",
-  //     "list_name_encoded": "",
-  //     "bestsellers_date": "",
-  //     "published_date": "",
-  //     "published_date_description": "",
-  //     "next_published_date": "",
-  //     "previous_published_date": "",
-  //     "display_name": "",
-  //     "normal_list_ends_at": 0,
-  //     "updated": "",
-  //     "books": []
-  //   });
-  // };
+  }, [genres, listName]);
 
     const returnBooksArray = () => {
       return list.books.map(book => {
@@ -68,21 +47,12 @@ const BookGenrePage = ({listName, genres, error}: Props) => {
         )
       })
     }
-  // console.log('this is selectedGenre.books', selectedGenre.books)
-
-  // const selectedGenreBooks =  list.books.map((book) => {
-  //   return (
-  //     <BookCard key={Math.random()} book={book} listName={listName}/>
-  //   );
-  // });
-  console.log('this is list', list)
 
   return (
     <>
     {list && 
     <section className='books-container'>
       {genreError ? <ErrorMessage /> : !list.books.length ? <img src={loadingIcon} className='loading-icon'/> : returnBooksArray()}
-      {/* <selectedGenreBooks /> */}
     </section>
      }
      </>
