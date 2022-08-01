0c406e8d46d2043bb5194c397241e95db89d1c89
Nick Orlov <ialwaysreply@gmail.com>
2022-08-01 03:14:13 -0700

    Add Artan Magic

--- src/BookGenrePage/BookGenrePage.tsx
+++ src/BookGenrePage/BookGenrePage.tsx
@@ -13,70 +13,81 @@ interface Props {
   genres: IState
 };
 
+let defaultList = {
+  "list_name": "",
+  "list_name_encoded": "",
+  "bestsellers_date": "",
+  "published_date": "",
+  "published_date_description": "",
+  "next_published_date": "",
+  "previous_published_date": "",
+  "display_name": "",
+  "normal_list_ends_at": 0,
+  "updated": "",
+  "books": []
+}
+
 const BookGenrePage = ({listName, genres, error}: Props) => {
 
-  const [ list, setList ] = useState({
-    "list_name": "",
-    "list_name_encoded": "",
-    "bestsellers_date": "",
-    "published_date": "",
-    "published_date_description": "",
-    "next_published_date": "",
-    "previous_published_date": "",
-    "display_name": "",
-    "normal_list_ends_at": 0,
-    "updated": "",
-    "books": []
-  });
+  const [ list, setList ] = useState(defaultList);
 
   const [genreError, setGenreError] = useState(error);
 
   const getListData = () => {
-    const selectedGenre: IList = genres.bookLists.find(genre => genre.list_name === listName)
-   return setList(selectedGenre)
+    console.log( 'list name', listName)
+      const selectedGenre: IList = genres.bookLists.find(genre => genre.list_name === listName)
+      setList(selectedGenre)
   };
 
   useEffect(() => {
+    console.log('list', list)
       getListData()
       // return clearEffect();
-  }, [listName]);
+  }, [genres]);
 
-  const clearEffect = () => {
-    setList({
-      "list_name": "",
-      "list_name_encoded": "",
-      "bestsellers_date": "",
-      "published_date": "",
-      "published_date_description": "",
-      "next_published_date": "",
-      "previous_published_date": "",
-      "display_name": "",
-      "normal_list_ends_at": 0,
-      "updated": "",
-      "books": []
-    });
-  };
+  // const clearEffect = () => {
+  //   setList({
+  //     "list_name": "",
+  //     "list_name_encoded": "",
+  //     "bestsellers_date": "",
+  //     "published_date": "",
+  //     "published_date_description": "",
+  //     "next_published_date": "",
+  //     "previous_published_date": "",
+  //     "display_name": "",
+  //     "normal_list_ends_at": 0,
+  //     "updated": "",
+  //     "books": []
+  //   });
+  // };
 
- 
+    const returnBooksArray = () => {
+      return list.books.map(book => {
+        return (
+          <BookCard key={Math.random()} book={book} listName={listName}/>
+        )
+      })
+    }
   // console.log('this is selectedGenre.books', selectedGenre.books)
-  const selectedGenreBooks = list.books.map(book => {
-    return (
-      <BookCard key={Math.random()} book={book} listName={listName}/>
-    )
-  })
 
   // const selectedGenreBooks =  list.books.map((book) => {
   //   return (
   //     <BookCard key={Math.random()} book={book} listName={listName}/>
   //   );
   // });
+  console.log('this is list', list)
 
   return (
+    <>
+    {list && 
     <section className='books-container'>
-      {/* {genreError ? <ErrorMessage /> : !list.books.length ? <img src={loadingIcon} className='loading-icon'/> : selectedGenreBooks} */}
-      <selectedGenreBooks />
+      {genreError ? <ErrorMessage /> : !list.books.length ? <img src={loadingIcon} className='loading-icon'/> : returnBooksArray()}
+      {/* <selectedGenreBooks /> */}
     </section>
-  );
+     }
+     </>
+     
+     );
 };
 
 export default BookGenrePage;
